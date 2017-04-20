ffdb316906f5328c53eaa7a7f52590abe319b099
Haibao Tang <htang@humanlongevity.com>
2017-04-20 12:03:10 -0700

    [public-server] Add clone_server.py

---
+++ public-server/clone_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Selectively clone contents from the private STR server.
+"""
+
+import errno
+import os
+import os.path as op
+import shutil
+import sys
+
+
+def read_contents(filename="git-ls.txt"):
+    """ Find all file contents
+    """
+    filenames = [x.strip() for x in open(filename)]
+    return [x for x in filenames]
+
+
+def cp_with_mkdir(src, target):
+    """ Copy file over, and automatically creating directories, like mkdir -p
+    """
+    if not op.exists(op.dirname(target)):
+        try:
+            os.makedirs(op.dirname(target))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    if op.exists(target):
+        print >> sys.stderr, "Skip {}".format(target)
+        return
+
+    # All good - proceed with copying
+    print >> sys.stderr, "Copy {} => {}".format(src, target)
+    shutil.copyfile(src, target)
+
+
+def main():
+    for filename in read_contents():
+        src = op.join("../server", filename)
+        target = op.join(".", filename)
+        cp_with_mkdir(src, target)
+
+
+if __name__ == '__main__':
+    main()
---
+++ public-server/set_ulimit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo kern.maxfiles=65536 | sudo tee -a /etc/sysctl.conf
+echo kern.maxfilesperproc=65536 | sudo tee -a /etc/sysctl.conf
+sudo sysctl -w kern.maxfiles=65536
+sudo sysctl -w kern.maxfilesperproc=65536
+ulimit -n 65536
