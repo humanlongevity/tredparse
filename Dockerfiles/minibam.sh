cf5e4164e58973d33fb2b8a70b0f7f66c509fc8c
Haibao Tang <htang@humanlongevity.com>
2017-04-21 16:59:41 -0700

    [docker] Add minibam.sh

---
+++ Dockerfiles/minibam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python -m jcvi.projects.str mini s3://hli-processed/processed/179258/isis_alignment_and_variant_calling/176449128/Data/Intensities/BaseCalls/Alignment/176449128_S1.bam t001_HD.bam
+python -m jcvi.projects.str mini s3://hli-processed/processed/147857/isis_alignment_and_variant_calling/187499847/Data/Intensities/BaseCalls/Alignment/187499847_S1.bam t002_DM1.bam
+python -m jcvi.projects.str mini s3://hli-processed/processed/330410/isaac_align/170500320_S1.bam t003_SCA17.bam
