2eca248ab5056911fabb92bfadb760e1931aeb68
Haibao Tang <htang@humanlongevity.com>
2017-04-20 17:40:23 -0700

    [public-server] Add run.sh for launching servers

---
+++ public-server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+(ROOT_URL=http://10.6.83.125:3100/tred meteor --port 3100 > meteor.log 2>&1 &)
---
+++ server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+(ROOT_URL=http://10.6.83.125:3000/tred meteor > meteor.log 2>&1 &)
